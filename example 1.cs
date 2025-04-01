4aec439daf5d945a6e0466827fd6788a29d5c495
Angelina Wangira <38504843+ANGELINATHECODER@users.noreply.github.com>
2025-04-01 17:00:11 +0300

    Add files via upload

---
+++ example 1.cs
@@ -0,0 +1,21 @@
+int value = 1;  // Initial value = 1
+
+value = value + 1;  // Increment using addition
+Console.WriteLine("First Increment: " + value);  // Output: 2
+
+value += 1;  // Increment using shorthand operator
+Console.WriteLine("Second Increment: " + value);  // Output: 3
+
+value++;  // Increment using postfix operator
+Console.WriteLine("Third Increment: " + value);  // Output: 4
+
+value = value - 1;  // Decrement using subtraction
+Console.WriteLine("First Decrement: " + value);  // Output: 3
+
+value -= 1;  // Decrement using shorthand operator
+Console.WriteLine("Second Decrement: " + value);  // Output: 2
+
+value--;  // Decrement using postfix operator
+Console.WriteLine("Third Decrement: " + value);  // Output: 1
+
+
